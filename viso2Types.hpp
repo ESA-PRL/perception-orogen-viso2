a9ddccff6ebd78f66482f1d61834046f4b3144a0
Javier Hidalgo Carrio <havyhidalgo@gmail.com>
2013-07-05 22:13:12 +0200

    viso2 Orogen depends now of frame_helper in order to visualize the intra_frame features correspondences. Note: frame_helper depends on opencv. Also the calibration parameters of rocks(frame_helper) are used instead to the viso2 for setting the intrinsic and extrinsic parameters. Viso2 types are used for the other values and the intrinsic and extrinsic parameters are set in the configureHook. This affects the .orogen and the yaml config file.

--- viso2Types.hpp
+++ viso2Types.hpp
@@ -13,22 +13,21 @@
 
 namespace viso2 {
 
-    /** Currently oroGen does not support class inheritance in ports/property defiitions **/
+    /** Currently oroGen does not support class inheritance in ports/property definitions **/
     /** This is a plain struct of the viso2 parameters which have inheritance **/
     /** Have a look at viso2/viso_stereo.h viso2/viso.h and viso2/matcher.h **/
 
     struct StereoOdometerParameters
     {
-        double  base;             // baseline (meters)
+        //double  base;           // baseline (meters) COMMENTED it will take the baseline from FrameHelper calibration parameters
         int32_t ransac_iters;     // number of RANSAC iterations
         double  inlier_threshold; // fundamental matrix inlier threshold
         bool    reweighting;      // lower border weights (more robust to calibration errors)
         Matcher::parameters         match;            // matching parameters
         VisualOdometry::bucketing   bucket;           // bucketing parameters
-        VisualOdometry::calibration calib;            // camera calibration parameters
+        //VisualOdometry::calibration calib;          // camera calibration parameters COMMENTED it will take values from FrameHelper calibration parameters
 
         StereoOdometerParameters () {
-          base             = 1.0;
           ransac_iters     = 200;
           inlier_threshold = 1.5;
           reweighting      = true;
